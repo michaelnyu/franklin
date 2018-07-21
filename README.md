6be1981f1160d0b9eadaad727aa9572a0f2f70ad
myu03 <myu03@oath.com>
2018-07-21 13:30:19 -0700

    finish

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # franklin
+Real-time stock data and more from your command line
+
+## Function
+1. Fetches current price of Equity Security (Stocks)
+2. Fetches real-time data about an Equity Security
+
+## Usage
+1. Price only
+    ```$ franklin --ticker=aapl```
+    ```$ franklin -t=aapl```
+
+2. Selected details
+    ```$ franklin --ticker=aapl --details```
+    ```$ franklin -t=aapl -d```
+
+## Setup
+- ensure working go environment
+- clone repository
+- cd franklin
+- go install
+- run ```$ franklin``` to your hearts content
+
+## Setting GO Path
+```$ export PATH=$PATH:/usr/local/go/bin```
+```$ export GOPATH=$HOME/go;```
+```$ export PATH=$PATH:$GOPATH/bin;```
--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"encoding/json"
+	"errors"
+	"log"
+)
+
+const (
+	apibase = "https://api.iextrading.com/1.0"
 )
 
 var (
@@ -13,7 +20,7 @@ var (
 )
 
 func fetchPrice(ticker string) (string, error) {
-	url := fmt.Sprintf("https://api.iextrading.com/1.0/stock/%s/price", ticker)
+	url := fmt.Sprintf("%s/stock/%s/price", apibase, ticker)
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -29,10 +36,58 @@ func fetchPrice(ticker string) (string, error) {
 	return string(body), nil
 }
 
+func fetchDetails(ticker string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("%s/stock/%s/stats", apibase, ticker)
+	var data map[string]interface{}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return data, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return data, err
+	}
+
+	// unmarshall response body
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        panic(err)
+    }
+
+	return data, nil
+}
+
+func parseDetails(data map[string]interface{}) (string, error) {
+	var res string
+
+	fields := []string{"week52high", "week52low", "dividendRate", "dividendYield", "beta", "returnOnEquity", "day200MovingAvg", "day50MovingAvg"}
+	err := errors.New("Error parsing details")
+
+	if val, ok := data["companyName"]; ok {
+		res = fmt.Sprintf("\t%s has a ...", val)
+	} else {
+		return "", err
+	}
+
+	for _, field := range fields {
+		if val, ok := data[field]; ok {
+			res += fmt.Sprintf("\n\t  %s of %f", field, val)
+		} else {
+			return "", err
+		}
+	}
+
+	return res, nil
+}
+
 func init() {
     flag.StringVar(&ticker, "ticker", "", "ticker to get data of")
 	flag.StringVar(&ticker, "t", "", "(alias) ticker to get data of")
-	flat.BoolVar(&)
+	flag.BoolVar(&detail, "detail", false, "show details")
+	flag.BoolVar(&detail, "d", false, "(alias) show details")
 
     flag.Usage = func() {
         flag.PrintDefaults()
@@ -42,10 +97,23 @@ func init() {
 }
 
 func main() {
-	price, err := fetchPrice(ticker)
-	if err != nil {
-		fmt.Println(err)
+	fmt.Println("💵")
+
+	if (detail) {
+		details, err := fetchDetails(ticker)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res, err := parseDetails(details)
+		fmt.Println(res)
+	} else {
+		price, err := fetchPrice(ticker)
+		if err != nil {
+			log.Fatal(err)
+		}
+		out := fmt.Sprintf("\tThe price of %s currently is: %s USD", ticker, price)
+		fmt.Println(out)
 	}
 
-	fmt.Println(price)
+	fmt.Println("💵")
 }
